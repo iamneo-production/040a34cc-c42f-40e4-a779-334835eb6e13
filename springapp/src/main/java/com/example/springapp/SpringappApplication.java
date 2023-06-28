859e6d8a098bf34ebecc9a166ed991ad040df726
SURIYADANASEKAR <suriyadanasekar18@gmail.com>
2023-06-28 13:48:40 +0000

    main

--- springapp/src/main/java/com/example/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/example/springapp/SpringappApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class SpringappApplication {
 
 	public static void main(String[] args) {

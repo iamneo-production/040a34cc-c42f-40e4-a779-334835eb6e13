f670388d1e5059bbb5a7bbb5ca5711fab6d1f145
SURIYADANASEKAR <suriyadanasekar18@gmail.com>
2023-07-03 13:48:55 +0000

    main

--- springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
@@ -1,9 +1,9 @@
-package main.java.com.examly.springapp.repository;
+package com.examly.springapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import main.java.com.examly.springapp.model.User;
+import com.examly.springapp.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>{

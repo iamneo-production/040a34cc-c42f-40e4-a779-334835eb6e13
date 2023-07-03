908c19d3660eeeb33f7d56cd67bf769233b4a3ad
SURIYADANASEKAR <suriyadanasekar18@gmail.com>
2023-07-03 10:43:04 +0000

    commit

--- springapp/src/main/java/com/examly/springapp/model/User.java
+++ springapp/src/main/java/com/examly/springapp/model/User.java
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-package com.examly.springapp.model;
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.util.Set;
-
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "users")
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    @Column(nullable = false, unique = true)
-    private String username;
-    @Column(nullable = false, unique = true)
-    private String email;
-    @Column(nullable = false)
-    private String password;
-
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinTable(name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
-    )
-    private Set<Role> roles;
-=======
 package main.java.com.examly.springapp.model;
 
 import javax.persistence.Entity;
@@ -65,8 +28,4 @@ public class User {
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	
-	
-	
->>>>>>> Education-loan-portal-karthi-0212
 }
--- springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-package com.examly.springapp.repository;
-
-
-import com.examly.springapp.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findByEmail(String email);
-
-    Optional<User> findByUsernameOrEmail(String username, String email);
-
-    Optional<User> findById(Long id);
-
-    Optional<User> findByUsername(String username);
-
-    Boolean existsByUsername(String username);
-
-    Boolean existsByEmail(String email);
-=======
 package main.java.com.examly.springapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -33,6 +10,6 @@ public interface UserRepository extends JpaRepository<User,Long>{
 
 	User findByUserId(long id);
 	
->>>>>>> Education-loan-portal-karthi-0212
+
 
 }

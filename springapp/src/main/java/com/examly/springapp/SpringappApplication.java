0953319201eae6f6bb396f24016351bac95e9ec0
karthi-0212 <karthikeyan01230123@gmail.com>
2023-07-03 10:26:45 +0000

    testing

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-
+//testing
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.examly"})
 @EnableJpaRepositories(basePackages = {"com.examly"})

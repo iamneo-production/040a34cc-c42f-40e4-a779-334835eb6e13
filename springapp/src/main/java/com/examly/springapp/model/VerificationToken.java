7f63d1a7e15f57f00c8232a496814125ae9aba58
SURIYADANASEKAR <suriyadanasekar18@gmail.com>
2023-07-23 14:53:32 +0000

    commit

---
+++ springapp/src/main/java/com/examly/springapp/model/VerificationToken.java
@@ -0,0 +1,85 @@
+package com.examly.springapp.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Calendar;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long token_id;
+    private String token;
+    private Date expirationTime;
+    private static final int EXPIRATION_TIME = 5;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    
+    public VerificationToken() {
+    	
+    }
+
+    public VerificationToken(String token, User user) {
+        super();
+        this.token = token;
+        this.user = user;
+        this.expirationTime = this.getTokenExpirationTime();
+    }
+
+    public VerificationToken(String token) {
+        super();
+        this.token = token;
+        this.expirationTime = this.getTokenExpirationTime();
+    }
+
+    public Date getTokenExpirationTime() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(new Date().getTime());
+        calendar.add(Calendar.MINUTE, EXPIRATION_TIME);
+        return new Date(calendar.getTime().getTime());
+    }
+
+	public Long getToken_id() {
+		return token_id;
+	}
+
+	public void setToken_id(Long token_id) {
+		this.token_id = token_id;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public Date getExpirationTime() {
+		return expirationTime;
+	}
+
+	public void setExpirationTime(Date expirationTime) {
+		this.expirationTime = expirationTime;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+    
+    
+}

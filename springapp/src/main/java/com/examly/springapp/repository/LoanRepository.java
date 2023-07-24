6755820eceaf6d15bd8cb480a3581998ccda6940
karthi-0212 <karthikeyan01230123@gmail.com>
2023-07-24 09:54:12 +0000

    cmt

--- springapp/src/main/java/com/examly/springapp/repository/LoanRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/LoanRepository.java
@@ -1,3 +1,21 @@
-public class LoanRepository {
-    
+package com.examly.springapp.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.examly.springapp.model.Loan;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+	
+	
+    List<Loan> findByLoanId(long id);
+
+    Loan findByUserId(long id);
+
+    Loan findByUserIdAndLoanId(Long userId, Long loanId);
+	
 }
--- springapp/src/main/java/com/examly/springapp/repository/PaymentRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/PaymentRepository.java
@@ -1,3 +1,11 @@
-public class PaymentRepository {
-    
+package com.examly.springapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.examly.springapp.model.Payment;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment,Long>{
+	
 }

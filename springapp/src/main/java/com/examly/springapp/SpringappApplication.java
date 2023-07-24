ad8b71ac0446ae66d40fa53a0b5804e4f0e3f9fb
karthi-0212 <karthikeyan01230123@gmail.com>
2023-07-24 09:06:14 +0000

    cmt

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -2,14 +2,8 @@ package com.examly.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.examly"})
-@EnableJpaRepositories(basePackages={"com.examly"})
-@EntityScan(basePackages={"com.examly"})
 public class SpringappApplication {
 
 	public static void main(String[] args) {

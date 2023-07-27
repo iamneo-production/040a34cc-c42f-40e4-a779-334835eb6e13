3529a20918f15062ad181c3593dc2a03e9530c91
Hari2316 <harishhdvp1623@gmail.com>
2023-07-27 10:06:28 +0000

    updated loanApplication3 repository

--- springapp/src/main/java/com/examly/springapp/repository/LoanApplication3Repository.java
+++ springapp/src/main/java/com/examly/springapp/repository/LoanApplication3Repository.java
@@ -5,16 +5,23 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.examly.springapp.model.LoanApplication3;
+import com.educationloan.PortalBackend.entity.LoanApplication3;
 
 @Repository
-public interface LoanApplication3Repository extends JpaRepository<LoanApplication3, Long>{
-	
-    Optional<LoanApplication3> findByTenthMarksheetFileName(String name);
-	
+public interface LoanApplication3Repository extends JpaRepository<LoanApplication3, Long> {
+
+	Optional<LoanApplication3> findByTenthMarksheetFileName(String name);
+
 	Optional<LoanApplication3> findByUserId(Long userId);
-	
-	Optional<LoanApplication3> findByUserIdAndId(Long userId, Long id);
+
+	Optional<LoanApplication3> findByLoanApplicationId(Long loanId);
+
+	Optional<LoanApplication3> findByUserIdAndLoanApplicationId(Long userId, Long id);
+
+	Optional<LoanApplication3> findByImageId(Long loanId);
+
+	Optional<LoanApplication3> findByUserIdAndImageId(Long userId, Long loanId);
 
 }
+
 

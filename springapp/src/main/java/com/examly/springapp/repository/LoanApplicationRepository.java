5c97c3fce7bd31d2eeef8632509b7148f4850496
Hari2316 <harishhdvp1623@gmail.com>
2023-07-27 09:47:21 +0000

    updated Loan Application model, repository, service and controller

--- springapp/src/main/java/com/examly/springapp/repository/LoanApplicationRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/LoanApplicationRepository.java
@@ -4,15 +4,21 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.examly.springapp.model.LoanApplication;
+import com.educationloan.PortalBackend.entity.LoanApplication;
 
 @Repository
 public interface LoanApplicationRepository extends JpaRepository<LoanApplication, Long> {
-	
+
 	List<LoanApplication> findByUserId(Long userId);
+
 	List<LoanApplication> findByStatus(String status);
+
 	Optional<LoanApplication> findByUserIdAndId(Long userId, Long id);
 	
+	@Query("SELECT t1 FROM LoanApplication t1 INNER JOIN LoanApplication3 t2 ON t1.userId=t2.userId")
+	List<LoanApplication> findAllByUserId();
+
 }

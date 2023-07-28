dc0a4ce686528ea41a5da5a015cf80f1a664b2c7
RithikaRamesh <rithikasri2002@gmail.com>
2023-07-28 18:07:12 +0000

    commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-=======
 import React from 'react';
 import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
 import Home from './pages/public/home/Home';
@@ -120,4 +93,3 @@ function UserRoutes() {
 }
 
 export default App;
->>>>>>> origin/main

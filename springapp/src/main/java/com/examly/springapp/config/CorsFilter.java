5b42c303b8a06abc00484e67b5a68dd03ca67713
2001priyanshuroy <priyanshuroy.codes2001@gmail.com>
2023-07-28 11:17:54 +0000

    commit

--- springapp/src/main/java/com/examly/springapp/config/CorsFilter.java
+++ springapp/src/main/java/com/examly/springapp/config/CorsFilter.java
@@ -1,9 +1,9 @@
-package com.example.springapp.config;
+package com.examly.springapp.config;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 
